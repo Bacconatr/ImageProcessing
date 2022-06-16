a5a445368ac4f60881e205eb09b623d065d7067d
capture120 <capture12020@gmail.com>
2022-06-16 18:10:50 -0400

    Commit before adding changes to the controller nad model (will attempt to move the load method to the controller).

--- src/imageprocessing/model/AdvancedImageProcessingModel.java
+++ src/imageprocessing/model/AdvancedImageProcessingModel.java
@@ -1,5 +1,6 @@
 package imageprocessing.model;
 
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.BufferedWriter;
 import java.io.File;
@@ -45,10 +46,9 @@ public class AdvancedImageProcessingModel extends ImageProcessingModelImpl
 
       for (int i = 0; i < height; i++) {
         for (int j = 0; j < width; j++) {
-          int r = image.getRGB(j, i) >> 16 & 0xff;
-          int g = image.getRGB(j, i) >> 8 & 0xff;
-          int b = image.getRGB(j, i) & 0xff;
-          listOfPixels[i][j] = new Pixel(r, g, b);
+          Color pixelColor = new Color(image.getRGB(j, i));
+          listOfPixels[i][j] = new Pixel(pixelColor.getRed(), pixelColor.getGreen(),
+                  pixelColor.getBlue());
         }
       }
 
@@ -68,24 +68,28 @@ public class AdvancedImageProcessingModel extends ImageProcessingModelImpl
     if (filePath.contains(".ppm")) {
       super.savePPM(filePath, imageName);
     } else {
-      Pixel[][] fileToSave = mapOfImages.getOrDefault(imageName, null);
-      if (fileToSave == null) {
+      Pixel[][] imageToSave = mapOfImages.getOrDefault(imageName, null);
+      if (imageToSave == null) {
         throw new IllegalArgumentException("The image you are trying to save does not exist.");
       }
       File file = new File(filePath);
+      // ImageIO.
 
       String fileType = filePath.split("\\.")[1];
+      int height = imageToSave.length;
+      int width = imageToSave[0].length;
 
-      BufferedImage image = new BufferedImage(fileToSave.length, fileToSave[0].length, 1);
-      for (Pixel[] pixels : fileToSave) {
-        for (int j = 0; j < fileToSave[0].length; j++) {
-
+      BufferedImage image = new BufferedImage(width, height, 1);
+      for (int i = 0; i < imageToSave.length; i++) {
+        for (int j = 0; j < imageToSave[0].length; j++) {
+          Color pixelColor = new Color(imageToSave[i][j].getRed(), imageToSave[i][j].getGreen(),
+                  imageToSave[i][j].getBlue());
+          image.setRGB(j, i, pixelColor.getRGB());
         }
       }
 
       try {
         ImageIO.write(image, fileType, file);
-
       } catch (IOException e) {
         throw new IllegalArgumentException("Error, the file path provided does not exist.");
       }
